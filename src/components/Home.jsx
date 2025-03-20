0e5869b4f825552c00452573f6aebfbfe36bae69
nanduaji <nanduaji64@gmail.com>
2025-03-20 22:31:31 +0530

    Enhance Home component styles with new background colors, improved layout, and additional service cards for better user experience

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap
 import styles from "./Home.module.css";
 import { Button } from "react-bootstrap";
-import { FaBed, FaUtensils, FaSpa, FaGift } from "react-icons/fa";
+import { FaBed, FaUtensils, FaSpa, FaGift, FaBook, FaCalendarCheck } from "react-icons/fa";
 
 function Home() {
 
@@ -51,6 +51,137 @@ function Home() {
                         <Button className={`${styles.oppositebutton} mt-2`}>
                             <FaGift className="me-2" /> <b>Special Offers</b>
                         </Button>
+
+                        <div className="mt-5" style={{ backgroundColor: "#e2edec", padding: "20px", borderRadius: "10px" }}>
+                            <h1 className={styles.title}>Unmatched Services to Elevate Your Stay</h1>
+                            {/* Cards Section */}
+                            <div className="card p-3">
+                                <div className="row g-0 align-items-center">
+                                    {/* Image on the left */}
+                                    <div className="col-md-4">
+                                        <img className="img-fluid rounded" src="pexels-photo-164595.jpeg" alt="Card image cap" />
+                                    </div>
+
+                                    {/* Text on the right */}
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Luxurious Accomodations</h5>
+                                            <p className="card-text">
+                                                Enjoy a range of room options designed for comfort and elegance, catering to all budgets and preferences.
+
+                                            </p>
+                                            <p className="card-text">
+                                                <h3 className="text-muted"> $ 50</h3>
+                                            </p>
+                                            <Button className={`${styles.oppositebutton} mt-2`}>
+                                                <FaCalendarCheck className="me-2" /> <b>Book Now</b>
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card p-3 mt-5">
+                                <div className="row g-0 align-items-center">
+                                    {/* Image on the left */}
+                                    <div className="col-md-4">
+                                        <img className="img-fluid rounded" src="finedining.webp" alt="Card image cap" />
+                                    </div>
+
+                                    {/* Text on the right */}
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Fine Dining</h5>
+                                            <p className="card-text">
+                                            Savor gourmet meals prepared by top chefs in our world-class restaurant, offering international cuisine.
+
+                                            </p>
+                                            <p className="card-text">
+                                                <h3 className="text-muted"> $ 50</h3>
+                                            </p>
+                                            <Button className={`${styles.oppositebutton} mt-2`}>
+                                                <FaCalendarCheck className="me-2" /> <b>Book Now</b>
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card p-3 mt-5">
+                                <div className="row g-0 align-items-center">
+                                    {/* Image on the left */}
+                                    <div className="col-md-4">
+                                        <img className="img-fluid rounded" src="spa.jpeg" alt="Card image cap" />
+                                    </div>
+
+                                    {/* Text on the right */}
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Spa & Wellness
+                                            </h5>
+                                            <p className="card-text">
+                                            Indulge in relaxing treatments at our upscale spa, designed to refresh and rejuvenate your body and mind.
+                                            </p>
+                                            <p className="card-text">
+                                                <h3 className="text-muted"> $ 50</h3>
+                                            </p>
+                                            <Button className={`${styles.oppositebutton} mt-2`}>
+                                                <FaCalendarCheck className="me-2" /> <b>Book Now</b>
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card p-3 mt-5">
+                                <div className="row g-0 align-items-center">
+                                    {/* Image on the left */}
+                                    <div className="col-md-4">
+                                        <img className="img-fluid rounded" src="conference.jpeg" alt="Card image cap" />
+                                    </div>
+
+                                    {/* Text on the right */}
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Conference Facilities
+                                            </h5>
+                                            <p className="card-text">
+                                            Host your events in our fully equipped conference rooms, suitable for business meetings and gatherings.
+                                            </p>
+                                            <p className="card-text">
+                                                <h3 className="text-muted"> $ 50</h3>
+                                            </p>
+                                            <Button className={`${styles.oppositebutton} mt-2`}>
+                                                <FaCalendarCheck className="me-2" /> <b>Book Now</b>
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card p-3 mt-5">
+                                <div className="row g-0 align-items-center">
+                                    {/* Image on the left */}
+                                    <div className="col-md-4">
+                                        <img className="img-fluid rounded" src="pexels-photo-164595.jpeg" alt="Card image cap" />
+                                    </div>
+
+                                    {/* Text on the right */}
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h5 className="card-title">Pool and Recreation
+                                            </h5>
+                                            <p className="card-text">
+                                            Relax and unwind at our stunning outdoor pool and recreation area, perfect for leisure and family fun.
+
+                                            </p>
+                                            <p className="card-text">
+                                                <h3 className="text-muted"> $ 50</h3>
+                                            </p>
+                                            <Button className={`${styles.oppositebutton} mt-2`}>
+                                                <FaCalendarCheck className="me-2" /> <b>Book Now</b>
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
 
                     </div>
